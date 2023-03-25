9d99aeb224b317791e824e7fbb060b0345513b92
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-03-25 17:32:51 +0200

    +

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,14 +2,12 @@ import { Input } from './Filter.styled'
 import PropTypes from 'prop-types';
 
 export default function Filter({ onFilterChange }) {
-
     return (
         <>  
             <label htmlFor="search"> Find contacts by name: </label>
             <Input onChange={onFilterChange}  type="text" id="search" />
         </>
     )
-
 }
 
 
